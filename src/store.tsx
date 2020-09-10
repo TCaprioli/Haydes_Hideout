30cc9f370bcd0ada4bcc66c6b55223c9588e8c24
Tcaprioli <Capriolityler@gmail.com>
2020-09-10 19:07:02 -0400

    removed vanilla redux store config

--- src/store.tsx
+++ src/store.tsx
@@ -1,17 +1,8 @@
-  
-// import { createStore,applyMiddleware } from 'redux'
-// import thunk from 'redux-thunk';
+
 import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from './rootReducer'
 import { navCountSlice } from './navbar/reducers/cartCount'
 import { inputSlice } from './navbar/reducers/inputSlice'
 
-
-// export function configureStore(){
-//   return createStore(
-//     rootReducer,applyMiddleware(thunk),
-//   );
-// }
 
 export const store = configureStore({
   reducer: {
