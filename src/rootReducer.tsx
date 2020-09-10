abb9e4b63782b1f5073346526458e5d56c6bebfe
Tcaprioli <Capriolityler@gmail.com>
2020-09-10 19:09:07 -0400

    removed rootreducer

--- src/rootReducer.tsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { navCountSlice } from './navbar/reducers/cartCount'
-
-export default combineReducers({
-  //add reducers below
-  counter: navCountSlice.reducer
-})
